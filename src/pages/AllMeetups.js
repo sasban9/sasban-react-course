e7145123b90659240f98b85e2667c5b296197b5b
sasban <azure.saswata@gmail.com>
2021-08-28 22:19:49 +0530

    Adding more React components.

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,5 @@
+import MeetupList from "../components/meetups/MeetupList";
+
 const DUMMY_DATA = [
     {
       id: 'm1',
@@ -6,28 +8,23 @@ const DUMMY_DATA = [
         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
       address: 'Meetupstreet 5, 12345 Meetup City',
       description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+        'This is a first, amazing meetup which you definitely should not miss.',
     },
     {
       id: 'm2',
       title: 'This is a second meetup',
       image:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
+      address: 'ShowupMall 15, 12345 Meetup City',
       description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+        'This is the second, amazing meetup which you definitely should not miss. It will be a lot of fun!',
     },
   ];
 
 function AllMeetupsPage() {
     return <section>
         <h1>All Meetups Page</h1>
-        {/* {[<li>Item 1</li>, <li>Item 2</li>]} */}
-        <ul>
-        {DUMMY_DATA.map((meetup) => {
-            return <li key={meetup.id}>{meetup.title}</li>
-        })}
-        </ul>
+            <MeetupList meetups={DUMMY_DATA} />
         </section>;
 }
 
