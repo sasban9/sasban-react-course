415ca3ef5e9787d1fb699a93df898d004e23e4a4
sasban <azure.saswata@gmail.com>
2021-08-28 18:59:59 +0530

    Passing functions as event props.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,14 +10,19 @@ function Todo(props) {
         // console.log(props.text + ' Clicked!');
         setModalIsOpen(true);
     }
+
+    function closeModalHandler() {
+        setModalIsOpen(false);
+    }
+
     return(
     <div className='card'>
       <h2>{props.text}</h2>
       <div className='actions'>
         <button className='btn' onClick={deleteHandler}>Delete</button>
       </div>
-        {modalIsOpen && <Modal />}
-        {modalIsOpen && <Backdrop />}
+        {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />}
+        {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
     );
 }
