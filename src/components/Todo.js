d4238567f8909f3c7b1bb542de7b74e0451f82a5
sasban <azure.saswata@gmail.com>
2021-08-28 18:48:02 +0530

    Introducing state.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,14 @@
+import { useState } from 'react';
+
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 function Todo(props) {
+    const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
     function deleteHandler() {
         // console.log(props.text + ' Clicked!');
+        setModalIsOpen(true);
     }
     return(
     <div className='card'>
@@ -8,6 +16,8 @@ function Todo(props) {
       <div className='actions'>
         <button className='btn' onClick={deleteHandler}>Delete</button>
       </div>
+        {modalIsOpen && <Modal />}
+        {modalIsOpen && <Backdrop />}
     </div>
     );
 }
